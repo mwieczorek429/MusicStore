f2bbbb5d281f1d8161fec58413fce051a254b98a
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2024-09-24 12:40:16 +0200

    Added shopping cart

---
+++ Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿namespace MusicStore.Models
+{
+	public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Album album, int quantity)
+        {
+            CartLine? line = lineCollection
+                .Where(p => p.Album.Id == album.Id)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Album = album,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity++;
+            }
+        }
+
+        public virtual void RemoveLine(Album album) => 
+            lineCollection.RemoveAll(l => l.Album.Id == album.Id);
+
+        public virtual decimal ComputeTotalValue() =>
+            lineCollection.Sum(e => e.Album.Price * e.Quantity);
+
+        public virtual void Clear() => 
+            lineCollection.Clear();
+
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+    }
+}
---
+++ Models/CartLine.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicStore.Models
+{
+	public class CartLine
+    {
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+        public int Quantity { get; set; }
+    }
+}
